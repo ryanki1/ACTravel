db8263051c0a693aab408824d6de608015c113ae
KebTen <contactkieran@hotmail.com>
2013-05-19 19:10:41 +0200

    YSLOW: Make fewer http requests

--- AddCon_TravellingSalesman/App_Start/BundleConfig.cs
+++ AddCon_TravellingSalesman/App_Start/BundleConfig.cs
@@ -21,50 +21,29 @@ namespace AddCon_TravellingSalesman
             bundles.Add(new ScriptBundle("~/bundles/geo").Include(
                     "~/Scripts/tutScript.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jsRender").Include(
-            //        "~/Scripts/jsRender.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-1.*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            //            "~/Scripts/jquery-ui*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.unobtrusive*",
-            //            "~/Scripts/jquery.validate*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
                         "~/Scripts/App/config.js",
                         "~/Scripts/App/common.js",
                         "~/Scripts/App/app.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jquerymobile").Include("~/Scripts/jquery.mobile*"));
-
-            bundles.Add(new StyleBundle("~/Content/oocss").Include("~/Content/oocss_all.css"
-                                                                    //,"~/Content/oocss_content"
+            bundles.Add(new StyleBundle("~/Content/thirdPartyCSS").Include(
+                                                                    "~/Content/oocss_all.css",
+                                                                    "~/Content/ts.css",
+                                                                    "~/Content/jquery.mobile*",
+                                                                    "~/Content/themes/base/jquery.ui.core.css",
+                                                                    "~/Content/themes/base/jquery.ui.resizable.css",
+                                                                    "~/Content/themes/base/jquery.ui.selectable.css",
+                                                                    "~/Content/themes/base/jquery.ui.accordion.css",
+                                                                    "~/Content/themes/base/jquery.ui.autocomplete.css",
+                                                                    "~/Content/themes/base/jquery.ui.button.css",
+                                                                    "~/Content/themes/base/jquery.ui.dialog.css",
+                                                                    "~/Content/themes/base/jquery.ui.slider.css",
+                                                                    "~/Content/themes/base/jquery.ui.tabs.css",
+                                                                    "~/Content/themes/base/jquery.ui.datepicker.css",
+                                                                    "~/Content/themes/base/jquery.ui.progressbar.css",
+                                                                    "~/Content/themes/base/jquery.ui.theme.css"
                                                                     ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/ts.css"));
-
-            bundles.Add(new StyleBundle("~/Content/mobilecss").Include("~/Content/jquery.mobile*"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
             bundles.Add(new StyleBundle("~/Content/appcss").Include("~/Content/app.css"));
         }
     }
